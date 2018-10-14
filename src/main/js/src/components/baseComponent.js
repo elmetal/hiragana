71fbd1c12d83648fe4fbe305626ebc9b3cfad95c
kotato <hakuyoka@gmail.com>
2018-10-14 20:44:42 +0900

    base Componentをexportする

--- src/main/js/src/components/baseComponent.js
+++ src/main/js/src/components/baseComponent.js
@@ -1,20 +1,20 @@
-const axiosBase = require('axios.js')
+import axiosBase from 'axios'
+
 const axios = axiosBase.create({
   baseURL: 'http://localhost:3030',
   headers: {
     'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest'
+    'X-Requested-With': 'XMLHttpRequest',
+    'Access-Control-Allow-Origin': 'localhost:8080'
   },
   responseType: 'json',
   timeout: 1000
-});
+})
 
 class BaseComponent {
-
-  constructor() {
-  }
-
   bindRequest(method, url) {
     return axios.request({method: method, url: url})
   }
 }
+
+export default BaseComponent
--- src/main/kotlin/com/mansei/hiragana/domain/item/ItemListController.kt
+++ src/main/kotlin/com/mansei/hiragana/domain/item/ItemListController.kt
@@ -1,6 +1,7 @@
 package com.mansei.hiragana.domain.item
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,7 +11,8 @@ class ItemListController {
     @Autowired
     lateinit var itemService: ItemService
 
-    @GetMapping("/item/list")
+    @CrossOrigin
+    @GetMapping("/item/list.json")
     fun getItemlist () :List<Item>{
         return itemService.listItems()
     }
